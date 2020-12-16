e994f3513703aef7a5b19c5ed69d350423f9f3ff
jkw <jkwchunjae@daum.net>
2020-12-16 10:56:35 +0900

    string.ToDate()

--- JkwExtensions/DateTimeExtensions.cs
+++ JkwExtensions/DateTimeExtensions.cs
@@ -50,5 +50,42 @@ namespace JkwExtensions
 
             return result;
         }
+
+        public static DateTime? ToDate(this string dateStr)
+        {
+            int year = 0, month = 0, day = 0;
+            var result = false;
+
+            try
+            {
+                if (dateStr.Length == 8)
+                {
+                    year = dateStr.Substring(0, 4).ToInt();
+                    month = dateStr.Substring(4, 2).ToInt();
+                    day = dateStr.Substring(6, 2).ToInt();
+                    result = true;
+                }
+                else if (dateStr.Length == 10)
+                {
+                    year = dateStr.Substring(0, 4).ToInt();
+                    month = dateStr.Substring(5, 2).ToInt();
+                    day = dateStr.Substring(8, 2).ToInt();
+                    result = true;
+                }
+
+                if (result)
+                {
+                    return new DateTime(year, month, day);
+                }
+                else
+                {
+                    return null;
+                }
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
