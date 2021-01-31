533c3be6b0699550a884dd5f5860e66076a66b9a
jkw <jkwchunjae@daum.net>
2021-02-01 00:31:51 +0900

    DateTime GetWeekday

--- JkwExtensions/DateTimeExtensions.cs
+++ JkwExtensions/DateTimeExtensions.cs
@@ -4,6 +4,19 @@ using System.Text;
 
 namespace JkwExtensions
 {
+    public enum DateLanguage
+    {
+        KR, EN
+    }
+
+    public enum WeekdayFormat
+    {
+        /// <summary> 월, Mon </summary>
+        D,
+        /// <summary> 월요일, Monday </summary>
+        DDD,
+    }
+
     public static class DateTimeExtensions
     {
         /// <summary>
@@ -51,6 +64,18 @@ namespace JkwExtensions
             return result;
         }
 
+        /// <summary> yyyymmdd hhmmss 형태의 str 을 DateTime으로 변환한다.  </summary>
+        public static DateTime ToDateTime(this string str)
+        {
+            return new DateTime(
+                str.Substring(0, 4).ToInt(),
+                str.Substring(4, 2).ToInt(),
+                str.Substring(6, 2).ToInt(),
+                str.Substring(9, 2).ToInt(),
+                str.Substring(11, 2).ToInt(),
+                str.Substring(13, 2).ToInt());
+        }
+
         public static DateTime? ToDate(this string dateStr)
         {
             int year = 0, month = 0, day = 0;
@@ -86,6 +111,33 @@ namespace JkwExtensions
             {
                 return null;
             }
+        }
+
+        public static string GetWeekday(this DateTime date, DateLanguage lang, WeekdayFormat format)
+        {
+            switch (lang)
+            {
+                case DateLanguage.KR:
+                    var dayKr = "일월화수목금토".Substring(date.DayOfWeek.ToString("d").ToInt(), 1);
+                    switch (format)
+                    {
+                        case WeekdayFormat.D:
+                            return dayKr;
+                        case WeekdayFormat.DDD:
+                            return dayKr + "요일";
+                    }
+                    break;
+                case DateLanguage.EN:
+                    switch (format)
+                    {
+                        case WeekdayFormat.D:
+                            return date.DayOfWeek.ToString("f").Substring(0, 3);
+                        case WeekdayFormat.DDD:
+                            return date.DayOfWeek.ToString("f");
+                    }
+                    break;
+            }
+            return string.Empty;
         }
     }
 }
