1600e47b525bc4b089e416f899526165560fbd2d
jkw <jkwchunjae@daum.net>
2021-04-30 18:30:41 +0900

    표준편차

---
+++ JkwExtensions/MathHelper.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace JkwExtensions
+{
+    public static class MathHelper
+    {
+        public static double StandardDeviation(this IEnumerable<double> source)
+        {
+            if (source.Any())
+                return 0;
+
+            double average = source.Average();
+            double sumOfSquaresOfDifferences = source.Sum(val => (val - average) * (val - average));
+            double sd = Math.Sqrt(sumOfSquaresOfDifferences / source.Count());
+
+            return sd;
+        }
+    }
+}
